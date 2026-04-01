9be6d1d49bfd3795377aaffa877ac285c257e372
Penny Wise I.T <steve@3dhub.au>
2026-04-01 11:24:06 +1000

    Add InstanceExplorer component

---
+++ src/features/build/InstanceExplorer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const InstanceExplorer = () => {
+  return (
+    <div>
+      <h1>Instance Explorer</h1>
+      {/* Your implementation here */}
+    </div>
+  );
+};
+
+export default InstanceExplorer;
